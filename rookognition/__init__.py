c3a70c49fabf8a622ce6fc423fd05515d5738f73
3d12 <3d12@users.noreply.github.com>
2024-07-29 01:19:27 -0500

    v1.1.3 - Reverting CI/CD Pipeline
    Trying to combine logic for tag and branch didn't work out as
    expected, I think I'll have to come back to this later and maybe
    implement something like the environments for this type of control.
    For now, anything that's tagged gets deployed I guess.

--- rookognition/__init__.py
+++ rookognition/__init__.py
@@ -20,7 +20,7 @@ from flask import Flask
 
 from werkzeug.middleware.proxy_fix import ProxyFix
 
-__version__ = "1.1.2"
+__version__ = "1.1.3"
 
 def create_app(test_config=None):
     # create and configure the app
